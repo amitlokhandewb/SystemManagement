c0be22fe91f8923bc9adf4982812e4a71ea0afdb
amitlokhandewb <amitlokhande909@gmail.com>
2024-07-24 17:52:35 +0530

    added webshell routes logic

--- webshell/src/SET-root-config.ts
+++ webshell/src/SET-root-config.ts
@@ -6,7 +6,42 @@ import {
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
 
-const routes = constructRoutes(microfrontendLayout);
+const userToken = localStorage.getItem('Token');
+
+const routesConfig = `
+<single-spa-router>
+  <main>
+    ${userToken ? `
+    <route default>
+      <application name="@SET/appbar"></application>
+      <div style="display: flex">
+        <application name="@SET/sidebar"></application>
+        <div style="display: flex">
+          <application name="@SET/monitor"></application>
+          <application name="@SET/assetinventory"></application>
+        </div>
+      </div>
+    </route>
+    <route path="maintainance-event">
+      <application name="@SET/appbar"></application>
+      <div style="display: flex">
+        <application name="@SET/sidebar"></application>
+        <div style="display: flex">
+          <application name="@SET/eventList"></application>
+        </div>
+      </div>
+    </route>` : ''}
+    <route path="login">
+      <application name="@SET/login"></application>
+    </route>
+    <route path="register">
+      <application name="@SET/register"></application>
+    </route>
+  </main>
+</single-spa-router>
+`;
+
+const routes = constructRoutes(routesConfig);
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
